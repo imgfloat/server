b783ab8b8d3b36c4b81de075e7162f7eb2a91550
Andreas Krühlmann <andreas@kruhlmann.dev>
2026-01-06 10:02:21 +0100

    Override eq for assetcontent

--- src/main/java/dev/kruhlmann/imgfloat/service/media/AssetContent.java
+++ src/main/java/dev/kruhlmann/imgfloat/service/media/AssetContent.java
@@ -1,3 +1,31 @@
 package dev.kruhlmann.imgfloat.service.media;
 
-public record AssetContent(byte[] bytes, String mediaType) {}
+import java.util.Arrays;
+import java.util.Objects;
+
+public record AssetContent(byte[] bytes, String mediaType) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AssetContent that = (AssetContent) o;
+        return Arrays.equals(bytes, that.bytes)
+                && Objects.equals(mediaType, that.mediaType);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(mediaType);
+        result = 31 * result + Arrays.hashCode(bytes);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AssetContent{" +
+                "bytes=" + Arrays.toString(bytes) +
+                ", mediaType='" + mediaType + '\'' +
+                '}';
+    }
+}
