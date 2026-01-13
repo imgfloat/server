559199d4e5fc5cf6e492bd674f8ea6a68a46ed2f
Andreas Krühlmann <andreas@kruhlmann.dev>
2026-01-13 21:11:47 +0100

    Removed client dependencies from shell.nix

--- shell.nix
+++ shell.nix
@@ -2,21 +2,9 @@
 
 pkgs.mkShell {
   packages = [
-    pkgs.electron
     pkgs.jdt-language-server
     pkgs.libxkbcommon
     pkgs.maven
-    pkgs.mesa
-    pkgs.nodePackages.prettier
-    pkgs.nodejs
-    pkgs.nss
-    pkgs.openbox
     pkgs.openjdk
-    pkgs.vulkan-loader
-    pkgs.wayland
-    pkgs.wayland-protocols
-    pkgs.weston
-    pkgs.xorg.xorgserver
-    pkgs.xwayland
   ];
 }
