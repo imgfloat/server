8581c6a01faa6f58c2db0f1870ec567205390b3a
Andreas Krühlmann <andreas@kruhlmann.dev>
2026-01-15 15:07:03 +0100

    Fix migration invalid syntax

--- src/main/resources/db/migration/V5__script_asset_layer.sql
+++ src/main/resources/db/migration/V5__script_asset_layer.sql
@@ -1,4 +1,4 @@
-ALTER TABLE script_assets ADD COLUMN IF NOT EXISTS z_index INTEGER NOT NULL DEFAULT 1;
+ALTER TABLE script_assets ADD COLUMN z_index INTEGER NOT NULL DEFAULT 1;
 
 UPDATE script_assets
 SET z_index = 1
